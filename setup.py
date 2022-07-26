4c4eafd7c35449f24bca364e26f863833256d5eb
Saurabh Naik <naiksaurabhd@gmail.com>
2022-07-26 14:02:53 +0530

    added dvc

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description= f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saurabh Naik",
+    description="This project explains about how to automate ML pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saurabhznaikz/simple-dvc-project",
+    author_email="naiksaurabhd@gmail.com",
+    package_dir={"":"src"},
+    packages=find_packages(where="src"),
+    license="GNU",
+    python_requires=">=3.6",
+    install_requires=[
+        'dvc',
+        # 'dvc[gdrive]',
+        # 'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
