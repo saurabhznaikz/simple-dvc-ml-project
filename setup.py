7e377d44c1382c8e9cd144220389d2cac7946466
Saurabh Naik <naiksaurabhd@gmail.com>
2022-07-26 20:02:35 +0530

    stage_01_load_and_save.py added

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/saurabhznaikz/simple-dvc-project",
     author_email="naiksaurabhd@gmail.com",
-    package_dir={"":"src"},
-    packages=find_packages(where="src"),
+    # package_dir={"":"src"},
+    # packages=find_packages(where="src"),
+    packages=["src"],
     license="GNU",
     python_requires=">=3.6",
     install_requires=[
